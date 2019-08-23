086b7b8ddbe43cb17661aa91a86e24300a895de0
Mila Evans <milaevans97@gmail.com>
2019-08-23 15:44:39 -0600

    mew

--- tile.js
+++ tile.js
@@ -1,4 +1,5 @@
 class Tile{
+<<<<<<< HEAD
   constructor(letter, x, y, width){
     this.width = width;
     this.letter = letter;
@@ -21,5 +22,18 @@ class Tile{
     fill(255, 255, 255);
     textSize(this.width / 1.25);
     text(this.letter, this.x + this.width / 4, this.y + this.width / 1.25);
+=======
+  constructor(letter){
+    this.width = 40;
+    this.letter = letter;
+  }
+
+  drawTile(x, y){
+    fill(255, 0, 0);
+    square(x, y, this.width);
+    fill(255, 255, 255);
+    textSize(this.width / 1.25);
+    text(this.letter, x + this.width / 4, y + this.width / 1.25);
+>>>>>>> b7e22c9 (mew)
   }
 }
